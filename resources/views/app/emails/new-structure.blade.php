fe80db30ecf75a9351b8e2b96db2ae75485f12c9
amstrong1 <lewisamstrongh@gmail.com>
2024-03-21 17:46:46 +0100

    url

--- resources/views/app/emails/new-structure.blade.php
+++ resources/views/app/emails/new-structure.blade.php
@@ -63,7 +63,7 @@
                                         </p>
                                         <p
                                             style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
-                                            <a href="/register">Créer mon compte administrateur</a>
+                                            <a href="{{ url()->current() . '/register' }}">Créer mon compte administrateur</a>
                                         </p>
                                         <p
                                             style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
