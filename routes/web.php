7ee0af6361f427217f98ef1135d9c4a24d19830a
amstrong1 <lewisamstrongh@gmail.com>
2024-03-28 16:09:59 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::middleware('auth')->group(function () {
 
     Route::resource('structures', StructureController::class);
 
-    Route::get('/logs', [LogController::class, 'index'])->name('logs.edit');
+    Route::get('/logs', [LogController::class, 'index'])->name('logs.index');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
